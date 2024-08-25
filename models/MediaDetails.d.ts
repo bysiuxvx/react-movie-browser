3c7319cc3753006b6515d3ba77136adf68764f47
Patryk B <byszek.patryk@gmail.com>
2024-08-26 01:53:39 +0200

    changes to names, abandoning 'movie' for 'media'

---
+++ models/MediaDetails.d.ts
@@ -0,0 +1,12 @@
+export type MediaDetails = {
+  Title: string
+  Year: string
+  imdbID: string
+  Poster: string
+  Genre: string
+  Director: String
+  Runtime: string
+  Plot: string
+  Type: string
+  Ratings: { Source: string; Value: string }[]
+}
