34bd6e986d82936fb0720f0602164050dc8bbde4
Patryk B <byszek.patryk@gmail.com>
2024-08-26 01:58:07 +0200

    backend route for fetching favorites

---
+++ app/api/favorites/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server"
+import { getAuth } from "@clerk/nextjs/server"
+import prisma from "../../utils/prisma-client"
+import createUser from "../../utils/create-user"
+
+export async function GET(req: NextRequest) {
+  const { userId } = getAuth(req)
+
+  if (!userId) {
+    return NextResponse.json({ error: "User ID is missing" }, { status: 400 })
+  }
+
+  try {
+    let user = await prisma.user.findUnique({
+      where: {
+        clerkId: userId,
+      },
+    })
+
+    if (!user) {
+      user = await createUser(userId)
+    }
+
+    const userWithFavorites = await prisma.user.findUnique({
+      where: { clerkId: userId },
+      include: {
+        favorites: {
+          include: {
+            rating: true,
+          },
+        },
+      },
+    })
+
+    return NextResponse.json(
+      { favorites: userWithFavorites?.favorites },
+      { status: 200 }
+    )
+  } catch (error: any) {
+    console.error("error: ", error)
+    return NextResponse.json({ error: error.message }, { status: 500 })
+  }
+}
