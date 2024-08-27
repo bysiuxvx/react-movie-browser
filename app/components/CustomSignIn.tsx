92220d6b96537b22cf874b32119b053f99ad1d97
Patryk B <byszek.patryk@gmail.com>
2024-08-27 17:22:14 +0200

    add custom button for  handling logging in

---
+++ app/components/CustomSignIn.tsx
@@ -0,0 +1,22 @@
+import { SignInButton } from "@clerk/nextjs"
+import React, { FC } from "react"
+import { Button } from "semantic-ui-react"
+
+interface CustomSignInButtonProps {
+  className?: string
+  children?: React.ReactNode
+}
+
+const CustomSignInButton: React.FC<CustomSignInButtonProps> = ({
+  children,
+  className,
+  ...props
+}) => {
+  return (
+    <SignInButton {...props}>
+      <Button className="sidebar-toggle">Sign in</Button>
+    </SignInButton>
+  )
+}
+
+export default CustomSignInButton
