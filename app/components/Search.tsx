0d4ac1b3188376f007baad674d86efa6fca28ea8
Patryk B <byszek.patryk@gmail.com>
2025-03-16 21:12:39 +0100

    add handling not found media

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -12,7 +12,7 @@ import { SearchItemTypes } from "../../enums/SearchItemTypes"
 const Search = () => {
   const [searchValue, setSearchValue] = useState<string>("")
   const [, setMediaList] = useAtom<MediaDetails[]>(mediaListAtom)
-  const [, setMediaNotFound] = useAtom<boolean | null>(mediaNotFoundAtom)
+  const [, setMediaNotFound] = useAtom<boolean>(mediaNotFoundAtom)
 
   const API_URL: string = `/api/search?query=${searchValue}`
   const DEBOUNCE_TIME: number = 500
