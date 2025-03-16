b2cdb15f821a017a2fe5d2c9a90a9b4925812ade
Patryk B <byszek.patryk@gmail.com>
2025-03-16 21:18:13 +0100

    add handling not found media

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -8,7 +8,6 @@ import { mediaListAtom, mediaNotFoundAtom } from "../../store/store"
 import { MediaDetails } from "../../models/MediaDetails"
 import debounce from "lodash.debounce"
 import { SearchItemTypes } from "../../enums/SearchItemTypes"
-
 const Search = () => {
   const [searchValue, setSearchValue] = useState<string>("")
   const [, setMediaList] = useAtom<MediaDetails[]>(mediaListAtom)
