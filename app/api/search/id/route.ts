f7fc4a736b284f9ea16233a6272a2701e346e9ed
Patryk B <byszek.patryk@gmail.com>
2025-06-20 14:43:42 +0200

    add redis caching to search\$id

--- app/api/search/id/route.ts
+++ app/api/search/id/route.ts
@@ -1,63 +1,65 @@
-import {NextRequest, NextResponse} from "next/server"
+import { NextRequest, NextResponse } from 'next/server';
+import Redis from 'ioredis';
+import { CacheTtl } from '../../../../enums/cache-ttl';
 
 interface OmdbResponse {
-  Response: string
-  Error?: string
-  [key: string]: any
+  Response: string;
+  Error?: string;
+  [key: string]: any;
 }
 
 export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url)
-  const movieId = searchParams.get("movieId")
+  const redisUrl = process.env.NEXT_PUBLIC_REDIS_URL!;
+  const redis = new Redis(redisUrl);
+
+  const { searchParams } = new URL(request.url);
+  const movieId = searchParams.get('movieId');
 
   if (!movieId) {
-    return NextResponse.json(
-      { error: "Movie ID is required" },
-      { status: 400 }
-    )
+    return NextResponse.json({ error: 'Movie ID is required' }, { status: 400 });
   }
 
-  const BASE_URL: string = process.env.OMDB_API_URL || 'https://www.omdbapi.com'
-  const API_URL: string = `${BASE_URL}/?i=${movieId}&apikey=${process.env.NEXT_PUBLIC_API_KEY}`
+  const cacheKey = `movie:${movieId}`;
+  const cachedData = await redis.get(cacheKey);
+
+  if (cachedData) {
+    return NextResponse.json(JSON.parse(cachedData), { status: 200 });
+  }
+
+  const BASE_URL: string = process.env.OMDB_API_URL || 'https://www.omdbapi.com';
+  const API_URL: string = `${BASE_URL}/?i=${movieId}&apikey=${process.env.NEXT_PUBLIC_API_KEY}`;
 
   try {
-    const response = await fetch(API_URL, {
-      next: { revalidate: 3600 }
-    })
+    const response = await fetch(API_URL);
 
     if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`)
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
 
-    const data: OmdbResponse = await response.json()
+    const data: OmdbResponse = await response.json();
 
-    if (data && data.Response === "True") {
-      return NextResponse.json(data, { status: 200 })
+    if (data && data.Response === 'True') {
+      await redis.set(cacheKey, JSON.stringify(data), 'EX', CacheTtl.ONE_WEEK);
+      return NextResponse.json(data, { status: 200 });
     } else {
       return NextResponse.json(
-        { error: data.Error || "Movie not found or API returned an error" },
+        { error: data.Error || 'Movie not found or API returned an error' },
         { status: 404 }
-      )
+      );
     }
   } catch (error: any) {
-    console.error("Error fetching media details:", error)
+    console.error('Error fetching media details:', error);
 
     if (error instanceof Error) {
-      if (error.message.includes("HTTP error")) {
-        return NextResponse.json(
-          { error: "Failed to fetch data from the API" },
-          { status: 500 }
-        )
+      if (error.message.includes('HTTP error')) {
+        return NextResponse.json({ error: 'Failed to fetch data from the API' }, { status: 500 });
       }
       return NextResponse.json(
-        { error: error.message || "An unexpected error occurred" },
+        { error: error.message || 'An unexpected error occurred' },
         { status: 500 }
-      )
+      );
     } else {
-      return NextResponse.json(
-        { error: "An unexpected error occurred" },
-        { status: 500 }
-      )
+      return NextResponse.json({ error: 'An unexpected error occurred' }, { status: 500 });
     }
   }
 }
